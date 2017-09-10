0724a23b6ebd424047df231c2110bd7363fa9217
María José Mendoza Rincón <mariajomen@gmail.com>
2017-09-10 13:43:39 -0500

    Creación de dialog Ver ejemplo en crear ejercicios y arreglo de estilos generales

--- src/app/actividad/actividad1/actividad1.component.ts
+++ src/app/actividad/actividad1/actividad1.component.ts
@@ -1,24 +1,27 @@
+import { Component, OnInit } from '@angular/core';
 import { Respuesta } from './../../model/respuesta.model';
 import { RespuestaService } from './../../service/respuesta.service';
 import { Profesor } from './../../model/profesor.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EjercicioService } from './../../service/ejercicio.service';
 import { Ejercicio } from './../../model/ejercicio.model';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-actividad1',
+  selector: 'zeuss-actividad1',
   templateUrl: './actividad1.component.html'
 })
 export class Actividad1Component implements OnInit {
   niveles: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   ejercicio: Ejercicio = new Ejercicio(0, '', '', '', '', 0);
-  message = "";
-  respuesta1 = new Respuesta(0, '', 0);
-  respuesta2 = new Respuesta(0, '', 0);
-  respuesta3 = new Respuesta(0, '', 0);
+  message = 'Agregue min 3 respuestas';
+  message2 = '';
+  respuesta = new Respuesta(0, '', 0);
+  respuestas: Array<Respuesta> = new Array;
   currentUser = new Profesor(0, '', '', '');
   correcta = 0;
+  actual: number = -1;
+  msjeTam = '';
+  guardoRes=false;
   constructor(
     private service: EjercicioService,
     private serviceRes: RespuestaService,
@@ -30,51 +33,61 @@ export class Actividad1Component implements OnInit {
     this.ejercicio.escenario = this.route.snapshot.params['escenario'];
     this.currentUser = JSON.parse(localStorage.getItem('USER'));
   }
-  guardar() {
-    if (this.correcta == 0) {
-      this.message = 'Seleccione la opción correcta';
+
+  change(i) {
+    if (this.actual != -1) {
+      this.respuestas[this.actual].correcta = 0;
+    }
+    this.respuestas[i].correcta = 1;
+    this.actual = i;
+    console.log(this.respuestas);
+  }
+
+  agregar() {
+    
+    if (this.respuesta.enunciado.length > 0) {
+      console.log(this.respuesta.enunciado);
+      this.msjeTam = '';
+      this.respuestas.push(this.respuesta);
+      this.respuesta = new Respuesta(0, '', 0);
+      
     }
     else {
-      if (this.correcta == 1) {
-        this.respuesta1.correcta = 1;
-      }
-      if (this.correcta == 2) {
-        this.respuesta2.correcta = 1;
-      }
-      if (this.correcta == 3) {
-        this.respuesta3.correcta = 1;
-      }
+      this.msjeTam = "Agregue la respuesta";
+    }
 
-      this.ejercicio.enunciado1 = this.ejercicio.enunciado1 + "__________" + this.ejercicio.enunciado2;
-      this.ejercicio.enunciado2 = '';
-      this.service.create(this.ejercicio).subscribe(
-        id => {
+  }
 
-          this.ejercicio.id = id
-          this.service.asoProActEj(this.ejercicio.id, this.currentUser.username, 1).subscribe(
-            success => {
-              this.serviceRes.create2(this.respuesta1, this.ejercicio.id).subscribe(
-                success => {
-                  this.serviceRes.create2(this.respuesta2, this.ejercicio.id).subscribe(
-                    success => {
-                      this.serviceRes.create2(this.respuesta3, this.ejercicio.id).subscribe(
-                        success => { this.router.navigate(['ejercicio/list']); },
-                        error => this.message = "Error: " + JSON.stringify(error)
-                      );
-                    },
+  guardar() {
+    this.guardoRes=true;
+    if (this.actual != -1) {
+      if (this.respuestas.length > 2) {
+        this.ejercicio.enunciado1 = this.ejercicio.enunciado1 + " ____ " + this.ejercicio.enunciado2;
+        this.ejercicio.enunciado2 = '';
+        this.service.create(this.ejercicio).subscribe(
+          id => {
+            this.ejercicio.id = id
+            this.service.asoProActEj(this.ejercicio.id, this.currentUser.username, 1).subscribe(
+              success => {
+                this.respuestas.forEach(element => {
+                  this.serviceRes.create2(element, this.ejercicio.id).subscribe(
+                    success => {  this.router.navigate(['ejercicio/list']); },
                     error => this.message = "Error: " + JSON.stringify(error)
                   );
-                },
-                error => this.message = "Error: " + JSON.stringify(error)
-              );
+                });
+              },
+              error => this.message = "Error: " + JSON.stringify(error)
+            );
+          },
+          error => this.message = "Error: " + JSON.stringify(error)
+        );
+      
+      }
 
-            },
-            error => this.message = "Error: " + JSON.stringify(error)
-          );
-        },
-        error => this.message = "Error: " + JSON.stringify(error)
-      );
-
+    }else{
+      this.message2='Eliga la respuesta correcta';
     }
   }
+
+
 }
--- src/app/ejercicio/ejercicio.component.ts
+++ src/app/ejercicio/ejercicio.component.ts
@@ -20,12 +20,24 @@ export class EjercicioComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    
     this.profesor = JSON.parse(localStorage.getItem('USER'));
     this.serviceEjercicio.findEjercicios(this.profesor.username)
       .subscribe(
-      ejercicios => this.ejercicios = ejercicios,
+      ejercicios => {this.ejercicios = ejercicios,
+        this.ejercicios.sort(this.compare);
+    },
       error => this.message = "Error: " + JSON.stringify(error)
       );
   }
+  compare(a,b) {
+    if (a.nivel < b.nivel)
+      return -1;
+    if (a.nivel > b.nivel)
+      return 1;
+    return 0;
+  }
+  
+  
 
 }
